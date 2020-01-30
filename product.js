b2a57310df4f6d4f5642f1f2d4e49a720ad82888
VIDYA A P S <35686464+apsvidyA@users.noreply.github.com>
2020-01-30 16:38:49 +0530

    mongodb1

--- product.js
+++ product.js
@@ -1,8 +1,3 @@
 const mongoose = require('mongoose');
 
-const productSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    name:String,
-    price:Number
-});
-module.exports=mongoose.model('Product',productSchema); 
+const productSchema = 
